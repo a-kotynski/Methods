fc1956452efd95e4feb6609fa7e884149232ba74
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-08 11:36:07 +0200

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2484612#content
+namespace Methods
+{
+    public class Point
+    {
+        // Signature of a Method
+        // * Name: Move()
+        // * Number and Type of parameters: int x, int y
+
+        public void Move(int x, int y)
+        {
+
+        }
+
+        // Overloading Methods
+        // * having a method with the same name, but different signatures
+        public void Move(int x, int y, int z) { }
+        public void Move(Point newLocation) { }
+        public void Move(Point newLocation, int speed) { }
+    }
+    public class Calculator
+    {
+        // A method with varying number of parameters
+
+        // proper way:
+        public int Add(int[] numbers) { }
+        // var result = calculator.Add(new int[] { 1, 2, 3, 4 }); // inefficient
+
+        // The Params Modifier
+        public int Add(params int[] numbers) { }
+        // var result = calculator.Add(1, 2, 3, 4);
+
+
+        // other, inefficient way:
+        public int Add(int n1, int n2) { }
+        public int Add(int n1, int n2, int n3) { }
+        public int Add(int n1, int n2, int n3, int n4) { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
